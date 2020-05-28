e22a15f4ece8b5643fe92416d1d777893f5e03ee
Ryan Harrington <rharrington.web@gmail.com>
2020-05-27 20:10:34 -0600

    admin command fix

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -10,16 +10,16 @@ class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.admins = info.bot_admins
-        self.console_channel = client.get_channel(707777532555952158)
 
     @commands.command()
     async def ban(self, msg):
         user = msg.message.author
         admin_role = get(user.guild.roles, name="Admin")
         mc_username = msg.message.content.replace("d!ban ", "")
+        console_channel = client.get_channel(707777532555952158)
 
         if admin_role in user.roles:
-            await self.console_channel.send(f"ban {mc_username}")
+            await console_channel.send(f"ban {mc_username}")
             await msg.send(f"**{mc_username}** has been banned.")
         else: 
             await msg.send("You do not have permission to use this command!")
@@ -28,9 +28,10 @@ class Admin(commands.Cog):
     async def unban(self, msg):
         user = msg.message.author
         mc_username = msg.message.content.replace("d!unban", "")
+        console_channel = client.get_channel(707777532555952158)
 
         if admin_role in user.roles: 
-            await self.console_channel.send(f"unban {mc_username}")
+            await console_channel.send(f"unban {mc_username}")
             await msg.send(f"**{mc_username}** has been unbanned.")
         else: 
             await msg.send("You do not have permission to use this command!")
