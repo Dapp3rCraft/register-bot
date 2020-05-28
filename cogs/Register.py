ce2a6a200dd7035148519b77f8bb1095dc09898e
dapp3rsrv <mc.choof15@gmail.com>
2020-05-27 23:40:01 -0600

    [Server Sync]

--- cogs/Register.py
+++ cogs/Register.py
@@ -11,8 +11,7 @@ class Register(commands.Cog):
     async def whitelist(self, msg, mc_username, user):
         channel = self.client.get_channel(707777532555952158)
         await channel.send(f"whitelist add {mc_username}")
-        await msg.send(f"Registered {str(user)} as {mc_username}")
-        await msg.send("Join the Minecraft server with ip: **dapp3rcraft.com**")
+        await msg.send(f"Registered {str(user)} as {mc_username}\nJoin the Minecraft server with ip: **dapp3rcraft.com**")
 
     @commands.command()
     async def register(self, msg):
