e738067670e99e4cb7c00f846ca2162c131da470
Ryan Harrington <rharrington.web@gmail.com>
2020-07-03 14:26:38 -0600

    fix: update help info

--- config/info.py
+++ config/info.py
@@ -1,36 +1,9 @@
 help = {
     "help": "Does this", 
     "ping": "Shows your ping",
+    "stats": "Shows the Dapp3rCraft Minecraft server latency",
     "register": "Allows new users access to the server",
     "ban": "Admins can ban users from the minecraft server",
     "unban": "Admins can unban users from the minecraft server",
     "kick": "Admins and Mods can kick users from the minecraft server"
 }
-
-bot_admins = [
-    "361247994630242304", 
-    "296862365503193098"
-]
-
-bot_members = [
-    "296862365503193098", 
-    "394887586386149387", 
-    "361247994630242304", 
-    "360211249029578752", 
-    "612112588649660426", 
-    "334082631354286083", 
-    "629530802941722654", 
-    "253329391445082112", 
-    "452578668665896977",
-    "300077792933052436",
-    "514281560321687562",
-    "586301764584865811",
-    "419017134375436298",
-    "587738842358874122",
-    "420741730313437187",
-    "692904937666904065",
-    "311689226695475202",
-    "693269783520477264",
-    "362772637585506305",
-    "660508153309560892"
-]
